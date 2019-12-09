9f1099a6614ad6def8e4cbf7c9eda09504dae2c7
Dorothy <dorothy@anttoweb.com>
2019-12-09 11:23:44 +0800

    Update footer.blade.php

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -14,5 +14,5 @@
 
     </div>
     <!-- Default to the left -->
-    <strong>Powered by <a href="https://github.com/z-song/laravel-admin" target="_blank">laravel-admin</a></strong>
+    <strong>Powered by <a href="https://www.anttoweb.com" target="_blank">安拓网络</a></strong>
 </footer>
